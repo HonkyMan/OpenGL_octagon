46c761e210de123614efd6ff9c992e1197ed6c95
Glimake <1k6j01@gmail.com>
2020-11-01 10:05:05 +0300

    added rotating, moving, zooming and texture

--- Lab2/Lab2.cpp
+++ Lab2/Lab2.cpp
@@ -1,9 +1,10 @@
 ﻿#include "GL/glut.h" 
+#include "GL/glaux.h"
 #include <vector>
 
 constexpr auto SIZE_OF_POLYGON = 8;
 constexpr auto PI = 3.14;
-constexpr auto INITIAL_ANGLE = 0;
+constexpr auto INITIAL_ANGLE = PI/6; // started angle from OY
 
 using namespace std;
 
@@ -24,10 +25,21 @@ GLubyte ColorR = 170,
 ColorG = 170,
 ColorB = 170;
 
+GLenum mode = GL_MODELVIEW;
+GLdouble splash[3] = {.0, .0, .0};//new GLdouble[3];
+GLdouble rotates[7] = { 0, 0, 0, 1, 0, 0, 0 };//new GLdouble[3];
+GLdouble zoom[3] = { 1, 1, 1 };
+
+FILE* file;
+GLubyte data54[54];
+int width = *(data54 + 18);
+int height = *(data54 + 22);
+GLubyte* pixels;
+
 struct type_point
 {
-	GLint x, y;
-	type_point(GLint _x, GLint _y)
+	GLfloat x, y;
+	type_point(GLfloat _x, GLfloat _y)
 	{
 		x = _x;
 		y = _y;
@@ -45,7 +57,7 @@ struct polygon
 	vector <type_point> Points;
 
 	double PHI = INITIAL_ANGLE;
-	double radius;
+	double radius = NULL;
 	GLubyte Red, Green, Blue;
 
 	polygon(GLubyte r = ColorR, GLubyte g = ColorG, GLubyte b = ColorB)
@@ -62,12 +74,20 @@ int pointCounter = octagons.size();
 double count_points_space(type_point a, type_point b);
 vector <type_point> count_octo_points(polygon figure, int n);
 
+vector<type_point> normolize(vector <type_point> Points, int n);
+
 #pragma endregion
 
 /* Головная программа */
 int main(int argc, char* argv[])
 {
 	glutInit(&argc, argv);
+
+	file = fopen("space.bmp", "rb");
+	fread(data54, 54, 1, file);
+	pixels = new GLubyte[width * height * 3];
+	fread(pixels, width * height * 3, 1, file);
+	fclose(file);
 
 	polygon buf(ColorR, ColorG, ColorB);
 	octagons.push_back(buf);
@@ -88,34 +108,74 @@ int main(int argc, char* argv[])
 /* Функция вывода на экран */
 void Display(void)
 {
+	GLuint tex; 
+	glGenTextures(1, &tex); 
+	gluBuild2DMipmaps(GL_TEXTURE_2D, 3, width, height, GL_BGR_EXT, GL_UNSIGNED_BYTE, pixels);
+
 	glClearColor(0.3, 0.3, 0.3, 1);
-	glClear(GL_COLOR_BUFFER_BIT);
-	glEnable(GL_LINE_SMOOTH);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glMatrixMode(mode);
+	glPushMatrix();
+	glLoadIdentity();
+
+	glTranslated(splash[0], splash[1], splash[2]);
+	// For rotate
+	glTranslated(rotates[4], rotates[5], rotates[6]);
+	glRotated(rotates[0], rotates[1], rotates[2], rotates[3]);
+	glTranslated(-rotates[4], -rotates[5], -rotates[6]);
+	//glTranslated(0, 0, 0);
+	//gluLookAt(0, 0, -10, 0, 0, 0, 0, 1, 0);
 	// glPointSize(1050);
+
+
+	// Рисование текстуры
+	glEnable(GL_TEXTURE_2D);
+	glColor3d(1, 1, 1);
+	glBindTexture(GL_TEXTURE_2D, tex);
+	//glBegin(GL_QUADS); 
+	glBegin(GL_QUADS);
 
 	for (int i = 0; i < octagons.size(); i++)
 	{
-		glColor3ub(octagons[i].Red, octagons[i].Green, octagons[i].Blue);
-		if ((i + 1) == octagons.size())
-		{
-			glLineWidth(10);
-			glColor3ub(GLubyte(255), GLubyte(255), GLubyte(255));
-		}
-		else
-		{
-			glLineWidth(5);
-		}
+		// Выделение акивного октогона
+		//glColor3ub(octagons[i].Red, octagons[i].Green, octagons[i].Blue);
+		//// Если активный полигон то делаем жирную линию и красим в белый
+		//if ((i + 1) == octagons.size())
+		//{
+		//	glLineWidth(10);
+		//	glColor3ub(GLubyte(255), GLubyte(255), GLubyte(255));
+		//}
+		//else
+		//{
+		//	glLineWidth(5);
+		//}
 
-		glBegin(GL_LINE_LOOP);
+		// Рисование октогона
+		//glEnable(GL_LINE_SMOOTH);
+		//glBegin(GL_LINE_LOOP);
+
+		//for (int j = 0; j < octagons[i].Points.size(); j++)
+		//{
+		//	glVertex2i(octagons[i].Points[j].x, octagons[i].Points[j].y);
+		//}
+
+		//glEnd();
+
+
+		vector<type_point> norm = normolize(octagons[i].Points, SIZE_OF_POLYGON);
 
 		for (int j = 0; j < octagons[i].Points.size(); j++)
 		{
-			glVertex2i(octagons[i].Points[j].x, octagons[i].Points[j].y);
+			glTexCoord2d(norm[j].x, norm[j].y); 
+			glVertex2d(octagons[i].Points[j].x, octagons[i].Points[j].y);
 		}
 
-		glEnd();
 	}
-
+	glEnd();
+	glDisable(GL_TEXTURE_2D);
+	glPopMatrix();
+	glutSwapBuffers();
 	glFinish();
 }
 
@@ -146,6 +206,46 @@ void Keyboard(unsigned char key, int x, int y)
 
 	if (key == 'b')
 		ColorB += 5;
+
+	// moving and zooming draws on canvas
+	if (key == '+')
+	{
+		octagons[pointCounter].radius += 1;
+		octagons[pointCounter].Points = count_octo_points(octagons[pointCounter], SIZE_OF_POLYGON);
+		//zoom[0] += 1;
+		//zoom[1] += 1;
+		//zoom[2] += 1;
+	}
+
+	if (key == '-')
+	{
+		octagons[pointCounter].radius -= 1;
+		octagons[pointCounter].Points = count_octo_points(octagons[pointCounter], SIZE_OF_POLYGON);
+		//zoom[0] -= 1;
+		//zoom[1] -= 1;
+		//zoom[2] -= 1;
+	}
+
+	if (key == '6')
+		splash[0] += 10;
+
+	if (key == '4')
+		splash[0] -= 10;
+
+	if (key == '8')
+		splash[1] += 10;
+
+	if (key == '2')
+		splash[1] -= 10;
+	// end moving draws
+
+	// rotating
+	if (key == '9')
+		rotates[0] += 10;
+
+	if (key == '7')
+		rotates[0] -= 10;
+	// end rotating
 
 	if (key == ' ')
 	{
@@ -182,11 +282,11 @@ void Keyboard(unsigned char key, int x, int y)
 	glutPostRedisplay();
 }
 
-/* Функцияобработкисообщенияотмыши */
+/* Функция обработки сообщения от мыши */
 void Mouse(int button, int state, int x, int y)
-{  /* клавишабыланажата, нонеотпущена */
+{  /* клавиша была нажата, но не отпущена */
 	if (state != GLUT_DOWN)
-		return;  /* новаяточкаполевомуклику */
+		return;  /* новая точка по левому клику */
 
 	if (button == GLUT_LEFT_BUTTON)
 	{
@@ -194,6 +294,11 @@ void Mouse(int button, int state, int x, int y)
 		{
 			type_point p(x, Height - y);
 			octagons[pointCounter].Center = p;
+			rotates[4] = octagons[pointCounter].Center.x;
+			rotates[5] = octagons[pointCounter].Center.y;
+			rotates[6] = 0;
+			//rotates[1] = octagons[pointCounter].Center.x;
+			//rotates[2] = octagons[pointCounter].Center.y;
 		}
 		else if( octagons[pointCounter].Center.x && octagons[pointCounter].Center.y)
 		{
@@ -351,6 +456,41 @@ vector <type_point> count_octo_points(polygon figure, int n = SIZE_OF_POLYGON)
 		GLint y = figure.Center.y + figure.radius * sin(figure.PHI + 2 * PI * i / n);
 		type_point p(x, y);
 		buf.push_back(p);
+	}
+
+	return buf;
+}
+
+vector<type_point> normolize(vector<type_point> Points, int n = SIZE_OF_POLYGON)
+{
+	vector<type_point> buf;
+
+	if (Points.size() == 0)
+	{
+		return buf;
+	}
+
+	double max_x = Points[0].x;
+	double max_y = Points[0].y;
+
+	// Find max values
+	for (int i = 0; i < n; i++)
+	{
+		if (max_x < Points[i].x)
+		{	
+			max_x = Points[i].x;
+		}
+
+		if (max_y < Points[i].y)	
+		{
+			max_y = Points[i].y;
+		}
+	}
+
+	// Normalizing
+	for (int i = 0; i < n; i++)
+	{
+		buf.push_back(type_point(Points[i].x / max_x, Points[i].y / max_y));
 	}
 
 	return buf;
